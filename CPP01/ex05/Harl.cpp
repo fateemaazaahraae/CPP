bda8fed693cabd7d23050924527189c15625f043
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2024-11-09 11:43:38 +0100

    done with cpp01/ex05

--- CPP01/ex05/Harl.cpp
+++ CPP01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: tiima <tiima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 08:42:16 by tiima             #+#    #+#             */
-/*   Updated: 2024/11/07 09:07:02 by tiima            ###   ########.fr       */
+/*   Updated: 2024/11/09 10:30:25 by tiima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,32 @@ void    Harl::debug( void ) {
 }
 
 void    Harl::info( void ) {
-    std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+    std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void    Harl::warning( void ) {
-    std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+    std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void    Harl::error( void ) {
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void    Harl::complain( std::string level )
+{
+    void        (Harl::*harl[])(void) = { &Harl::debug, &Harl::info, &Harl::warning, &Harl::error };
+    std::string levels[] = { "debug", "info", "warning", "error" };
+    int         i;
+
+    i = 0;
+    while (i < 4)
+    {
+        if (levels[i] == level)
+        {
+            (this->*harl[i])();
+            return ;
+        }
+        i++;
+    }
+    std::cout << "Unknown level!" << std::endl;
 }
--- CPP01/ex05/main.cpp
+++ CPP01/ex05/main.cpp
@@ -6,7 +6,18 @@
 /*   By: tiima <tiima@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 08:42:20 by tiima             #+#    #+#             */
-/*   Updated: 2024/11/07 08:42:21 by tiima            ###   ########.fr       */
+/*   Updated: 2024/11/09 10:02:59 by tiima            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+# include "Harl.hpp"
+
+int main()
+{
+    Harl Harl1;
+
+    Harl1.complain("debug");
+    Harl1.complain("warning");
+    Harl1.complain("info");
+    Harl1.complain("error");
+}
