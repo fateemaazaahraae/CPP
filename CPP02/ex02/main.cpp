10e421b78955eb36960b9e50469e925aeac2617e
fateemaazaahraae <fatimazahraebazaz3@gmail.com>
2024-11-22 14:31:22 +0100

    done with cpp02/ex02

---
+++ CPP02/ex02/main.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tiima <tiima@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/19 15:30:29 by tiima             #+#    #+#             */
+/*   Updated: 2024/11/22 14:14:53 by tiima            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Fixed.hpp"
+
+int main( void )
+{
+    Fixed a;
+    Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+    std::cout << a << std::endl;
+    std::cout << ++a << std::endl;
+    std::cout << a << std::endl;
+    std::cout << a++ << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    std::cout << Fixed::max( a, b ) << std::endl;
+    return 0;
+}
+
